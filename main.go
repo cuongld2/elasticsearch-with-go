5b8e865d546eafea94bf2eb44512494f9b25485b
cuongld2 <ledinhcuong99@gmail.com>
2020-10-12 16:18:21 +0700

    Add example code to interact elasticsearch in Go

---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"elasticsearch-with-go/es"
+	_ "elasticsearch-with-go/es"
+	"elasticsearch-with-go/model"
+	_ "elasticsearch-with-go/model"
+	"encoding/json"
+	"fmt"
+	"github.com/olivere/elastic/v7"
+)
+
+func main() {
+
+	ctx := context.Background()
+	esclient, err := es.GetESClient()
+	if err != nil {
+		fmt.Println("Error initializing : ", err)
+		panic("Client fail ")
+	}
+
+	//creating student object
+	//newStudent := model.Gene{
+	//	GeneId:         "Gopher doe",
+	//	GeneName:          "Testi",
+	//	Description: "Testing",
+	//}
+	//
+	//dataJSON, err := json.Marshal(newStudent)
+	//js := string(dataJSON)
+	//_, err = esclient.Index().
+	//	Index("gene").
+	//	BodyJson(js).
+	//	Do(ctx)
+	//
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Println("[Elastic][InsertProduct]Insertion Successful")
+
+	var genes []model.Gene
+
+	searchSource := elastic.NewSearchSource()
+	searchSource.Query(elastic.NewMatchQuery("gene-name", "Testi"))
+
+	/* this block will basically print out the es query */
+	queryStr, err1 := searchSource.Source()
+	queryJs, err2 := json.Marshal(queryStr)
+
+	if err1 != nil || err2 != nil {
+		fmt.Println("[esclient][GetResponse]err during query marshal=", err1, err2)
+	}
+	fmt.Println("[esclient]Final ESQuery=\n", string(queryJs))
+	/* until this block */
+
+	searchService := esclient.Search().Index("gene").SearchSource(searchSource)
+
+	searchResult, err := searchService.Do(ctx)
+	if err != nil {
+		fmt.Println("[ProductsES][GetPIds]Error=", err)
+		return
+	}
+
+	for _, hit := range searchResult.Hits.Hits {
+		var gene model.Gene
+		err := json.Unmarshal(hit.Source, &gene)
+		if err != nil {
+			fmt.Println("[Getting Genes][Unmarshal] Err=", err)
+		}
+
+		genes = append(genes, gene)
+	}
+
+	if err != nil {
+		fmt.Println("Fetching gene fail: ", err)
+	} else {
+		for _, s := range genes {
+			fmt.Printf("Gene found GeneID: %s, GeneName: %s, Description: %s \n", s.GeneId, s.GeneName, s.Description)
+		}
+	}
+
+}
---
+++ model/gene.go
@@ -0,0 +1,7 @@
+package model
+
+type Gene struct {
+	GeneId      string `json:"gene-id"`
+	GeneName    string `json:"gene-name"`
+	Description string `json:"description"`
+}
